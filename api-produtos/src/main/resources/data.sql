c5c23183f77aa167e0dbedb4eb26e98df362237c
pedro <pedroeugeniocavalcanti@gmail.com>
2022-08-31 16:37:18 -0300

    Script para popular tabelas no api-produtos

---
+++ api-produtos/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into tb_produtos(cor, descricao, nome) values('Azul', 'teste', 'Tenis nike');
+insert into tb_produtos(cor, descricao, nome) values('preto', 'teste', 'tenis olk');
+insert into tb_produtos(cor, descricao, nome) values('Vermelho', 'teste', 'Camisa polo');
+insert into tb_produtos_estoque(produto_id, data_alteracao, quantidade, usuario) values(1, '2022-10-11', 10, 'pedro');
+insert into tb_produtos_estoque(produto_id, data_alteracao, quantidade, usuario) values(2, '2022-05-25', 5, 'maria');
+insert into tb_produtos_estoque(produto_id, data_alteracao, quantidade, usuario) values(3, '2021-01-15', 2, 'joao');
